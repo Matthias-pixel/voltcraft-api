21b4a42975f8cb049ac60316464ac9af5425c885
Ideaonic703 <vinc.gold01@gmail.com>
2022-06-01 13:55:04 +0200

    Delete build.sh for safety reasons

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-rm -rf /home/pi/Desktop/voltcraft-api/python-flask-api
-java -jar /home/pi/Desktop/swagger-codegen-3.0.34/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-  -i /home/pi/Desktop/voltcraft-api/api.yaml \
-  -l python-flask \
-  -o /home/pi/Desktop/voltcraft-api/python-flask-api
-cd /home/pi/Desktop/voltcraft-api/python-flask-api
-pip3 install -r requirements.txt
